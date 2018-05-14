3fc36378876f8954afed7038feacae32b8ae1f2f
Russell King <rmk+kernel@armlinux.org.uk>
2018-05-14 14:20:21 +0100

    ARM: spectre-v2: harden user aborts in kernel space
    In order to prevent aliasing attacks on the branch predictor,
    invalidate the BTB or instruction cache on CPUs that are known to be
    affected when taking an abort on a address that is outside of a user
    task limit:
    
    Cortex A8, A9, A12, A17, A73, A75: flush BTB.
    Cortex A15, Brahma B15: invalidate icache.
    
    If the IBE bit is not set, then there is little point to enabling the
    workaround.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Boot-tested-by: Tony Lindgren <tony@atomide.com>
    Reviewed-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/include/asm/cp15.h
+++ arch/arm/include/asm/cp15.h
@@ -49,6 +49,9 @@
 
 #ifdef CONFIG_CPU_CP15
 
+#define BPIALL                         __ACCESS_CP15(c7, 0, c5, 6)
+#define ICIALLU                                __ACCESS_CP15(c7, 0, c5, 0)
+
 extern unsigned long cr_alignment;	/* defined in entry-armv.S */
 
 static inline unsigned long get_cr(void)
