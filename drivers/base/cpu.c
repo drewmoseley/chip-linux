f034d41fb6133e38b7759ab496071e1d223d28b5
Thomas Rohloff <v10lator@myway.de>
2018-06-11 11:05:42 +0200

    Revert "driver core: cpu: use put_device() if device_register fail"
    This reverts commit 508b04dd38118224a08d22b6c40163fd849985ad as we get a kernel NULL pointer dereference.

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -371,10 +371,10 @@ int register_cpu(struct cpu *cpu, int num)
 	if (cpu->hotpluggable)
 		cpu->dev.groups = hotplugable_cpu_attr_groups;
 	error = device_register(&cpu->dev);
-	if (error) {
-		put_device(&cpu->dev);
-		return error;
-	}
+	if (!error)
+		per_cpu(cpu_sys_devices, num) = &cpu->dev;
+	if (!error)
+		register_cpu_under_node(num, cpu_to_node(num));
 
 	return error;
 }
